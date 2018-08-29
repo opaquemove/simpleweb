241fe911618822c4992f89c0eea34d8db5e22f87
Apple <apple@ApplenoMacBook.local>
2018-08-29 14:04:40 +0900

    29.Aug.2018-1

---
+++ index.js
@@ -0,0 +1,16 @@
+'use strict'
+var   ipaddress = process.env.OPENSHIFT_NODEJS_IP || "0.0.0.0";
+var   port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+
+var http = require('http');
+var server = http.createServer( ( req, res ) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain' );
+  res.end( 'Hello World' );
+});
+
+server.on( 'listening', () => {
+  console.log('listening');
+});
+
+server.listen( port, ipaddress );
