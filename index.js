b0fd6cc29e35ed79c65d0692dd31782a33c59445
Apple <apple@ApplenoMacBook.local>
2018-09-29 15:50:29 +0900

    29.Sep.2018-3

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict'
 var   ipaddress = process.env.OPENSHIFT_NODEJS_IP || "0.0.0.0";
-var   port      = proces.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var   port      = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
 var http = require('http');
 var server = http.createServer( ( req, res ) => {
