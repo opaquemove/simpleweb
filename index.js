9684b7799a5b3097471d4894dfaaf5e554209684
opaquemove <40309433+opaquemove@users.noreply.github.com>
2018-10-24 14:23:00 +0900

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var http = require('http');
 var server = http.createServer( ( req, res ) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain' );
-  res.end( 'Hello MASATO.NAKANISHI 7.Sep.2018-3.' );
+  res.end( 'Hello MASATO.NAKANISHI 24.Dec.2018.' );
 });
 
 server.on( 'listening', () => {
